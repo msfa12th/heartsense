44306c302080aac73b7d00aeaf3997d7bb058564
Mary Brown <maryadbrown@yahoo.com>
2020-01-31 17:22:32 -0500

    data loaded into db

--- data-load/schema.sql
+++ data-load/schema.sql
@@ -26,17 +26,109 @@ CREATE TABLE behavior_risk (
   BreakoutCategoryID int,
   BreakOutID int,
   LocationID int,
-  GeoLocation text,
+  GeoLocation text
 );
 
-CREATE TABLE billing_info (
-  billing_id INT PRIMARY KEY NOT NULL,
-  street_address TEXT,
-  state TEXT,
-  username TEXT
+CREATE TABLE heart_mortality (
+    Year int,
+    LocationAbbr text,
+    LocationDesc text,
+    GeographicLevel text,
+    DataSource text,
+    Class text,
+    Topic text,
+    Data_Value text,
+    Data_Value_Unit text,
+    Data_Value_Type text,
+    Data_Value_Footnote_Symbol text,
+    Data_Value_Footnote text,
+    StratificationCategory1 text,
+    Stratification1 text,
+    StratificationCategory2 text,
+    Stratification2 text,
+    TopicID text,
+    LocationID text, 
+    Y_lat text,
+    X_lon text,
+    Georeference text,
+    States text,
+    Counties text
 );
 
-CREATE TABLE payment_info (
-  billing_id INT PRIMARY KEY NOT NULL,
-  cc_encrypted TEXT
+CREATE TABLE heart_ml_cleveland (
+    age int,
+    sex int,
+    cp int,
+    trestbps int,
+    chol int,
+    fbs int,
+    restecg int,
+    thalach int,
+    exang int,
+    oldpeak int,
+    slope int,
+    ca int,
+    thal int,
+    num int
 );
+
+
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c0 TO age;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c1 TO sex;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c2 TO cp;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c3 TO trestbps;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c4 TO chol;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c5 TO fbs;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c6 TO restecg;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c7 TO thalach;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c8 TO exang;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c9 TO oldpeak;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c10 TO slope;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c11 TO ca;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c12 TO thal;
+
+ALTER TABLE heart_ml_cleveland 
+RENAME COLUMN _c13 TO num;
+
+update heart_ml_cleveland
+set thal=null
+where thal='?';
+
+update heart_ml_cleveland
+set ca=null
+where ca='?';
+
+ALTER TABLE heart_ml_cleveland
+ALTER COLUMN ca TYPE double precision USING ca::double precision;
+
+ALTER TABLE heart_ml_cleveland
+ALTER COLUMN thal TYPE double precision USING thal::double precision;
+
+
+
