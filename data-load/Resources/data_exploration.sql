623bd0d575cccd3208572e60a74696842e5b5d52
Mary Brown <maryadbrown@yahoo.com>
2020-02-01 08:41:19 -0500

    initial data exploration

--- data-load/Resources/data_exploration.sql
+++ data-load/Resources/data_exploration.sql
@@ -1,7 +1,44 @@
 -- Behavioral risk factors for hear disease dataset
 --https://data.world/cdc/behavioral-risk-factor-heart/workspace/query?queryid=sample-0
+
 select count(*) 
 from heart_behavior_risk; --35004 records
+
+select  "Year", count(*)
+from heart_behavior_risk
+group by "Year"
+order by "Year";
+-- Year count
+-- 2011	"13186"
+-- 2012	"4316"
+-- 2013	"13186"
+-- 2014	"4316"
+
+select  "Year","Category",
+count(*)
+from heart_behavior_risk
+where "Category" = 'Cardiovascular Diseases'
+group by "Year","Category"
+order by "Category","Year";
+
+-- 2011	"Cardiovascular Diseases"	"2526"
+-- 2013	"Cardiovascular Diseases"	"2526"
+-- 2011	"Risk Factors"	"10660"
+-- 2012	"Risk Factors"	"4316"
+-- 2013	"Risk Factors"	"10660"
+-- 2014	"Risk Factors"	"4316"
+
+
+--** MY CONCLUSION this dataset is insufficient
+--** We need to go from data.world to the original source CDC
+-- https://catalog.data.gov/dataset/behavioral-risk-factor-data-heart-disease-amp-stroke-prevention
+-- https://catalog.data.gov/dataset/national-health-interview-survey-nhis-national-cardiovascular-disease-surveillance-data
+-- https://data.cdc.gov/Heart-Disease-Stroke-Prevention/National-Health-Interview-Survey-NHIS-National-Car/fwns-azgu
+
+
+
+
+
 
 -- Cleveland Heart Disease Machine Learning dataset
 -- https://archive.ics.uci.edu/ml/datasets/Heart+Disease
