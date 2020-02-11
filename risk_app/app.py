4a3984e8c3faaf530f895f4d6c7ca6c0cd1c8754
Mary Brown <maryadbrown@yahoo.com>
2020-02-11 16:20:51 -0500

    working app

--- risk_app/app.py
+++ risk_app/app.py
@@ -3,13 +3,41 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import *
 from sqlalchemy.ext.automap import automap_base
-from sklearn.svm import *
-from sklearn.externals import joblib
 
 import os
 import json
 import numpy as np
+from sklearn.svm import *
+# from sklearn.externals 
+import joblib
+
 import pickle
+
+import tensorflow as tf
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+from tensorflow.keras.optimizers import RMSprop,Nadam,Adadelta,Adam
+from tensorflow.keras.layers import BatchNormalization,LeakyReLU
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+import seaborn as sns
+import scipy.stats as stats
+import sklearn
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+import warnings
+from tensorflow.keras.utils import to_categorical
+from numpy import loadtxt
+from keras.models import load_model
+# import keras.backend.tensorflow_backend as tb
+# tb._SYMBOLIC_SCOPE.value = True
+
+# import pickle
 
 app = Flask(__name__)
 
@@ -74,9 +102,8 @@ def data():
     # return jsonify(results)
     return render_template("index.html", rows=results)
 
-
 @app.route('/resultCR',methods = ['POST'])
-def resultCR():
+def resultCRSVC():
     if request.method == 'POST': 
         patient_info = request.form.to_dict()
    
@@ -91,7 +118,6 @@ def resultCR():
         myFeatures[6]=round(703*myFeatures[5]/(myFeatures[6]*myFeatures[6]),2)
 
         final_features = [np.array(myFeatures)]
-        # loaded_model = joblib.load(open("svc_best_model_cardio.pkl","rb"))
         loaded_model = joblib.load(open("model_svc.pkl","rb"))
         result = loaded_model.predict(final_features)
 
@@ -103,7 +129,42 @@ def resultCR():
         else:
             prediction='NOT a Heart Disease Risk'
             
-        # return render_template("predictCardioRisk.html",prediction=result,)
+
+        return render_template("predictCardioRisk.html",status="results",prediction=prediction,result=result[0],patient=patient_info,myBMI=myFeatures[6],myChol=myList[cholesterolValue])
+
+
+
+@app.route('/resultCRNN',methods = ['POST'])
+def resultCRNN():
+    if request.method == 'POST': 
+        patient_info = request.form.to_dict()
+   
+        # get 9 input values
+        int_features = [int(x) for x in request.form.values()]
+
+        # reorder the 7 features
+        feature_order = [1,4,5,6,0,3,2]
+        myFeatures = [int_features[i] for i in feature_order]
+
+        #calculate BMI 703*lbs/in2
+        myFeatures[6]=round(703*myFeatures[5]/(myFeatures[6]*myFeatures[6]),2)
+
+        final_features = [np.array(myFeatures)]
+        final_shape = np.reshape(final_features,(1,-1))
+        Best_model = load_model('NN_model.h5')
+
+        result = Best_model.predict_proba(final_shape)
+        # result = Best_model.predict(final_shape)
+
+        myList = ['Empty','Normal (< 200)', 'Borderline High (200-239)', 'High (240 and higher)']
+        cholesterolValue = int(patient_info['cholesterol'])
+
+        if int(result)==1:
+            prediction='Heart Disease Risk'
+        else:
+            prediction='NOT a Heart Disease Risk'
+            
+
         return render_template("predictCardioRisk.html",status="results",prediction=prediction,result=result[0],patient=patient_info,myBMI=myFeatures[6],myChol=myList[cholesterolValue])
 
 
